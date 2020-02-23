998e874b56cabfc07bbbd2317919d847ae2ebb4b
BVA1000 <bennyvonachen@gmail.com>
2020-02-23 23:10:11 +0000

    Finish static pages

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,34 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end 
+end   
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-end
+
